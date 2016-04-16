288427f100609f8eaba0955d478dfcd3c7553066
cestinger <celeste.stinger@gmail.com>
2016-04-16 10:29:39 -0500

    more files

---
+++ celeste/chapter2/battleship.js
@@ -0,0 +1,29 @@
+var location1 = Math.floor(Math.random() * 5);
+var location2 = location1 + 1;
+var location3 = location1 + 2;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while (! isSunk) {
+  guess = prompt("Ready, aim, fire!! (enter a number from 0-6):");
+  if (guess < 0 || guess > 6) {
+    alert("You need to enter a valid number from 0 to 6");
+  } else { 
+    guesses = guesses + 1;
+    if ((guess == location1) || (guess == location2) || (guess == location3)) {
+      hits = hits + 1;
+      alert("HIT");
+      if (hits == 3) {
+        isSunk = true;
+        alert("You sank my battleship!");
+      }
+    } else {
+      alert("MISS");
+    }
+  }
+}
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
